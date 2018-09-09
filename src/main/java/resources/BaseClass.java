ee9c03e31bfcddd84fb9d64739371a7e1b7f6ade
Kandaguru Baskaran <42104999+kandaguru@users.noreply.github.com>
2018-09-10 10:06:11 +1200

    Latest Commit

--- src/main/java/resources/BaseClass.java
+++ src/main/java/resources/BaseClass.java
@@ -1,6 +1,7 @@
 package resources;
 
 import java.io.File;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
--- src/test/java/E2EPackage/LoginTC2.java
+++ src/test/java/E2EPackage/LoginTC2.java
@@ -51,7 +51,7 @@ public class LoginTC2 extends BaseClass {
 	
 		//driver.get(prop.getProperty("ChromeClear"));	
 		
-		//clearCache();	
+		clearCache();	
 		
 		
 		
